87e9e14b49d8365e1ce1ae9bf1cf745957b6813e
Yong-sik <om77901@gmail.com>
2025-01-23 18:12:41 +0900

    초기커밋

---
+++ src/main/java/com/example/easypickSkinCareApi/EasypickSkinCareApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.easypickSkinCareApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.example.easypickSkinCareApi"})
+public class EasypickSkinCareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasypickSkinCareApiApplication.class, args);
+	}
+
+}
